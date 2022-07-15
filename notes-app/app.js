9b4a8ca6f14acf95e78abd700c1921f211c73df9
kevin-mehta <kevin.h.mehta@gmail.com>
2022-07-15 21:06:58 +0530

    Implemented fs.appendFileSync().

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 
 try {
   fs.writeFileSync('Notes.txt', 'This file has been created by NodeJS!');
+  fs.appendFileSync(
+    'Notes.txt',
+    'The file content has been appended by NodeJS.'
+  );
+  fs.appendFileSync('Notes.txt', '\nI live in Mumbai.');
 } catch (err) {
   console.error('Error while writing to the file. \nerr: ', err);
 }
