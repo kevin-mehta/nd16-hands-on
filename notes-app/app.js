6e604990089b9ff89a345fda4cd468d01fbe386e
kevin-mehta <kevin.h.mehta@gmail.com>
2022-07-16 09:43:38 +0530

    Commented "fs" related code.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,17 @@
 /**
  * 3.2. Importing NodeJS core modules.
  */
-const fs = require('fs');
-try {
-  fs.writeFileSync('Notes.txt', 'This file has been created by NodeJS!');
-  fs.appendFileSync(
-    'Notes.txt',
-    'The file content has been appended by NodeJS.'
-  );
-  fs.appendFileSync('Notes.txt', '\nI live in Mumbai.');
-} catch (err) {
-  console.error('Error while writing/appending to the file. \nerr: ', err);
-}
+// const fs = require('fs');
+// try {
+//   fs.writeFileSync('Notes.txt', 'This file has been created by NodeJS!');
+//   fs.appendFileSync(
+//     'Notes.txt',
+//     'The file content has been appended by NodeJS.'
+//   );
+//   fs.appendFileSync('Notes.txt', '\nI live in Mumbai.');
+// } catch (err) {
+//   console.error('Error while writing/appending to the file. \nerr: ', err);
+// }
 
 /**
  * 3.3. Importing your own files.
